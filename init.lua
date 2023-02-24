935df409251e7a6e2b722915b2f6abf2e6310b95
Dennis Jutemark <djutemark@gmail.com>
2023-02-24 08:16:16 +0100

    Add leap

--- init.lua
+++ init.lua
@@ -84,6 +84,9 @@ require('packer').startup(function(use)
     end,
   })
 
+  -- 2023-02-24
+  use 'ggandor/leap.nvim'
+
   -- Add custom plugins to packer from ~/.config/nvim/lua/custom/plugins.lua
   local has_plugins, plugins = pcall(require, 'custom.plugins')
   if has_plugins then
