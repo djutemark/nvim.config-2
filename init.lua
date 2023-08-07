513b0167d099c60551812bee639c3cb817ca92a4
Dennis Jutemark <dennis.jutemark@cdab.eu>
2023-08-07 07:40:06 +0200

    help renamed to vimdoc fix

--- init.lua
+++ init.lua
@@ -284,7 +284,7 @@ vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { de
 -- See `:help nvim-treesitter`
 require('nvim-treesitter.configs').setup {
   -- Add languages to be installed here that you want installed for treesitter
-  ensure_installed = { 'c', 'cpp', 'c_sharp', 'go', 'lua', 'python', 'rust', 'typescript', 'svelte', 'help', 'vim' },
+  ensure_installed = { 'c', 'cpp', 'c_sharp', 'go', 'lua', 'python', 'rust', 'typescript', 'svelte', 'vimdoc', 'vim' },
 
   highlight = { enable = true },
   indent = { enable = true, disable = { 'python' } },
