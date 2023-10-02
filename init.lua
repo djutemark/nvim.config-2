e958e337e949df720973059967d70910dfcde0f2
Dennis Jutemark <djutemark@gmail.com>
2023-10-02 21:13:16 +0200

    mirate to indent-blankline v3

--- init.lua
+++ init.lua
@@ -221,9 +221,10 @@ require('Comment').setup()
 
 -- Enable `lukas-reineke/indent-blankline.nvim`
 -- See `:help indent_blankline.txt`
-require('indent_blankline').setup {
-  char = '┊',
-  show_trailing_blankline_indent = false,
+require "ibl".setup {
+  indent = {
+    char = '┊',
+  },
 }
 
 -- Gitsigns
